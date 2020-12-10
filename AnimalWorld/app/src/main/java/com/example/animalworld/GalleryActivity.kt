888210fa771da045dfa35351b24858de27dcb530
OWNER-PC\Owner <yansad24@gmail.com>
2020-12-10 12:32:14 +0900

    ギャラリーボタン追加

--- AnimalWorld/app/src/main/java/com/example/animalworld/GalleryActivity.kt
+++ AnimalWorld/app/src/main/java/com/example/animalworld/GalleryActivity.kt
@@ -3,11 +3,40 @@ package com.example.animalworld
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_zoo.*
+
 
 class GalleryActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+
+        //アニマルワールドボタンを押すと解説画面に遷移
+        button_to_animalworld.setOnClickListener {
+            val intent = Intent(this, ZooActivity::class.java)
+            startActivity(intent)
+        }
+        button_to_turtle.setOnClickListener {
+            val intent = Intent(this, ZooActivity::class.java)
+            startActivity(intent)
+        }
+        button_to_giraffe.setOnClickListener {
+            val intent = Intent(this, ZooActivity::class.java)
+            startActivity(intent)
+        }
+        button_to_cat.setOnClickListener {
+            val intent = Intent(this, ZooActivity::class.java)
+            startActivity(intent)
+        }
+        button_to_rion.setOnClickListener {
+            val intent = Intent(this, ZooActivity::class.java)
+            startActivity(intent)
+        }
+        button_to_elephant.setOnClickListener {
+            val intent = Intent(this, ZooActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
