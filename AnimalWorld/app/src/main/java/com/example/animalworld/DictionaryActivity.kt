45876022c3838bdd63c309f441f3b2626e679d4a
yutotototo <youtopia3921daobfj@gmail.com>
2020-12-18 02:52:05 +0900

    drawableに説明用画像の追加、引数により5種の動物の内容を表示するように設定。

--- AnimalWorld/app/src/main/java/com/example/animalworld/DictionaryActivity.kt
+++ AnimalWorld/app/src/main/java/com/example/animalworld/DictionaryActivity.kt
@@ -3,24 +3,46 @@ package com.example.animalworld
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_dictionary.*
 
 class DictionaryActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dictionary)
+
         var an = 1 //判別結果を代入
-        when(an){
-            1 -> print("Cat")
-            //ネコの解説を表示
-            2 -> print("Elephant")
-            //ネコの解説を表示
-            3 -> print("Giraffe")
-            //ネコの解説を表示
-            4 -> print("Lion")
-            //ネコの解説を表示
-            5 -> print("Turtle")
-            //ネコの解説を表示
+        var aName = findViewById<TextView>(R.id.name_view)
+        var aContent = findViewById<TextView>(R.id.content_view)
+        var aImage = findViewById<ImageView>(R.id.image_view)
+
+        when(an) {
+            1 -> {
+                aName.setText(R.string.cat)
+                aImage.setImageResource(R.drawable.catimage)
+                aContent.setText(R.string.catcon)
+            }//ネコの解説を表示
+            2 ->{
+                aName.setText(R.string.elephant)
+                aImage.setImageResource(R.drawable.catimage)
+                aContent.setText(R.string.catcon)
+            }//ゾウの解説を表示
+            3 ->{
+                aName.setText(R.string.giraffe)
+                aImage.setImageResource(R.drawable.giraffeimage)
+                aContent.setText(R.string.giraffecon)
+            }//キリンの解説を表示
+            4 ->{
+                aName.setText(R.string.lion)
+                aImage.setImageResource(R.drawable.lionimage)
+                aContent.setText(R.string.lioncon)
+            }//ライオンの解説を表示
+            5 ->{
+                aName.setText(R.string.turtle)
+                aImage.setImageResource(R.drawable.turtleimage)
+                aContent.setText(R.string.turtlecon)
+            }//カメの解説を表示
         }
 
         //ボタンを押すとメイン画面に遷移
